9bb442f5852b560b8fce01d738a089ee67d36e99
Omar Mohamed <64428610+skittoo@users.noreply.github.com>
2023-06-24 05:15:24 +0300

    Finishing the first project

--- first_example/app.py
+++ first_example/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -19,5 +19,40 @@ stores = [
 ]
 
 @app.get("/store")
-def get_store():
+def get_stores():
     return { "stores":  stores}
+
+
+@app.post("/store")
+def create_store():
+    request_data = request.get_json()
+    new_store = {"name": request_data["name"], "items": []}
+    stores.append(new_store)
+
+    return new_store, 201 # 201 means: okay, i have acepted the new data.
+
+
+@app.post("/store/<string:name>/item")
+def create_item(name= None):
+    request_data = request.get_json()
+    for store in stores:
+        if store["name"] == name:
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
+            store["items"].append(new_item)
+            return new_item, 201
+    return {"message": "Store not found"}, 404
+
+@app.get("/store/<string:name>")
+def get_store(name= None):
+    for store in stores:
+        if store["name"] == name:
+            return store, 200
+    return {"message": "store not found"}, 404
+
+@app.get("/store/<string:name>/item")
+def get_items(name= None):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}, 200
+    return {"message": "store not found"}, 404
+    
